fb6e611940862b2e0fe377bacb7eed4c902e43bf
AdamLevy2001 <adle0001@stud.ek.dk>
2026-04-27 09:39:31 +0200

    Har lavet alle package

--- src/test/java/com/example/projekt3_gruppe_4/Projekt3Gruppe4ApplicationTests.java
+++ src/test/java/com/example/projekt3_gruppe_4/Projekt3Gruppe4ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Projekt3Gruppe4ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
