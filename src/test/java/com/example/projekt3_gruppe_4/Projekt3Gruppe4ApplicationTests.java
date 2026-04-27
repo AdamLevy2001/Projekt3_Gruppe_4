ebf799c8cf345ca2c25d78b99a1cc8b54d05bbfd
AdamLevy2001 <adle0001@stud.ek.dk>
2026-04-27 09:25:35 +0200

    Initial commit

---
+++ src/test/java/com/example/projekt3_gruppe_4/Projekt3Gruppe4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projekt3_gruppe_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekt3Gruppe4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
