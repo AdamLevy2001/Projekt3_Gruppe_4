b3cb26872a765ace23bb19889f9f74abece1bf76
AdamLevy2001 <adle0001@stud.ek.dk>
2026-05-08 11:26:58 +0200

    Har lavet en del ændringer med repository plus jeg har lavet frontend og controller, med at se skaderapporter

--- src/main/java/com/example/projekt3_gruppe_4/model/DamageReport.java
+++ src/main/java/com/example/projekt3_gruppe_4/model/DamageReport.java
@@ -10,10 +10,20 @@ public class DamageReport {
 
     private LocalDate created_at;
 
-    public DamageReport(int id, int lease_id, LocalDate created_at) {
+    private String brand;
+
+
+    private String model;
+
+    private String customerName;
+
+    public DamageReport(int id, int lease_id, LocalDate created_at, String brand, String model, String customerName) {
         this.id = id;
         this.lease_id = lease_id;
         this.created_at = created_at;
+        this.brand=brand;
+        this.model=model;
+        this.customerName=customerName;
     }
 
     public int getId() {
@@ -38,5 +48,29 @@ public class DamageReport {
 
     public void setCreated_at(LocalDate created_at) {
         this.created_at = created_at;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
     }
 }
