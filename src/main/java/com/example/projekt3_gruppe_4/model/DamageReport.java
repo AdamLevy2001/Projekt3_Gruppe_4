5577e599b9b8b348d911060dc2862b5e573d9fcf
MFWahlgren <mawa0006@stud.ek.dk>
2026-05-08 12:34:17 +0200

    Removed irrelevant attributes

--- src/main/java/com/example/projekt3_gruppe_4/model/DamageReport.java
+++ src/main/java/com/example/projekt3_gruppe_4/model/DamageReport.java
@@ -3,27 +3,14 @@ package com.example.projekt3_gruppe_4.model;
 import java.time.LocalDate;
 
 public class DamageReport {
-
     private int id;
-
     private int lease_id;
-
     private LocalDate created_at;
 
-    private String brand;
-
-
-    private String model;
-
-    private String customerName;
-
-    public DamageReport(int id, int lease_id, LocalDate created_at, String brand, String model, String customerName) {
+    public DamageReport(int id, int lease_id, LocalDate created_at) {
         this.id = id;
         this.lease_id = lease_id;
         this.created_at = created_at;
-        this.brand=brand;
-        this.model=model;
-        this.customerName=customerName;
     }
 
     public int getId() {
@@ -48,29 +35,5 @@ public class DamageReport {
 
     public void setCreated_at(LocalDate created_at) {
         this.created_at = created_at;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public String getCustomerName() {
-        return customerName;
-    }
-
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
     }
 }
