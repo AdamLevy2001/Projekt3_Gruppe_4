a8adcd8c0f9fce5f7504cdb8f103c1a2ea714ada
MFWahlgren <mawa0006@stud.ek.dk>
2026-05-08 12:35:37 +0200

    Naming change

--- src/main/java/com/example/projekt3_gruppe_4/service/DamageReportService.java
+++ src/main/java/com/example/projekt3_gruppe_4/service/DamageReportService.java
@@ -1,6 +1,6 @@
 package com.example.projekt3_gruppe_4.service;
 
-import com.example.projekt3_gruppe_4.model.DamageReport;
+import com.example.projekt3_gruppe_4.model.DamageReportView;
 import com.example.projekt3_gruppe_4.repository.DamageReportRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,7 +13,7 @@ public class DamageReportService {
     @Autowired
     DamageReportRepository damageReportRepository;
 
-    public List<DamageReport> getAllDamageReports() {
+    public List<DamageReportView> getAllDamageReports() {
         return damageReportRepository.getAllDamageReports();
     }
 }
