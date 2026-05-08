e85c7cd0c9c2ebd286b2aa4facf8159b47ab5bc9
AdamLevy2001 <adle0001@stud.ek.dk>
2026-05-08 11:26:58 +0200

    Har lavet en del ændringer med repository plus jeg har lavet frontend og controller, med at se skaderapporter

--- src/main/java/com/example/projekt3_gruppe_4/repository/DamageReportRepository.java
+++ src/main/java/com/example/projekt3_gruppe_4/repository/DamageReportRepository.java
@@ -7,7 +7,10 @@ import org.springframework.stereotype.Repository;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class DamageReportRepository {
@@ -28,5 +31,38 @@ public class DamageReportRepository {
         } catch (SQLException e) {
             throw new RuntimeException("Fejl ved gemning af skadesrapport", e);
         }
+    }
+
+    public List<DamageReport> getAllDamageReports() {
+        String sql = "SELECT dr.id, dr.lease_id, dr.created_at, " +
+                "c.brand, c.model, " +
+                "CONCAT(cu.first_name, ' ', cu.last_name) AS customerName " +
+                "FROM damageReports dr " +
+                "INNER JOIN leases l ON dr.lease_id = l.id " +
+                "INNER JOIN cars c ON l.carVehicle_no = c.vehicle_no " +
+                "INNER JOIN customers cu ON l.customer_id = cu.id";
+       List<DamageReport> damageReports = new ArrayList<>();
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                DamageReport damageReport = new DamageReport(
+                        resultSet.getInt("id"),
+                        resultSet.getInt("lease_id"),
+                        resultSet.getDate("created_at").toLocalDate(),
+                        resultSet.getString("brand"),
+                        resultSet.getString("model"),
+                        resultSet.getString("customerName")
+
+                );
+                damageReports.add(damageReport);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Fejl ved visning af skaderapport", e);
+        }
+
+        return damageReports;
     }
 }
