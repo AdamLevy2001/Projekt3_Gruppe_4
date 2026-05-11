4e8681519524da393ef9f9118af5050280bd021d
MFWahlgren <mawa0006@stud.ek.dk>
2026-05-11 12:12:04 +0200

    Changed defalutPage and hasAccess

--- src/main/java/com/example/projekt3_gruppe_4/model/ForretningsudviklerUser.java
+++ src/main/java/com/example/projekt3_gruppe_4/model/ForretningsudviklerUser.java
@@ -7,13 +7,13 @@ public class ForretningsudviklerUser extends User {
 
     @Override
     public String getDefaultPage() {
-        return "forretningsudvikler/udlejede-biler";
+        return "forretningsudvikler/dashboard";
     }
 
     @Override
     public boolean hasAccess(String page) {
         return switch (page) {
-            case "forretningsudvikler/udlejede-biler", "forretningsudvikler/skadesrapporter", "forretningsudvikler/skadesrapport" -> true;
+            case "forretningsudvikler/udlejede-biler", "forretningsudvikler/skadesrapporter", "forretningsudvikler/skadesrapport", "forretningsudvikler/dashboard" -> true;
             default -> false;
         };
     }
